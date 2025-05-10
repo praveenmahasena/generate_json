2eb333744507f63aba248445e23ca9ae21379329
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-10 15:41:31 -0400

    adding in feature read file

--- internal/app.go
+++ internal/app.go
@@ -1,23 +1,41 @@
 package internal
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
 
 	"github.com/praveenmahasena/generate_json/internal/cli"
+	"github.com/praveenmahasena/generate_json/internal/jsonreader"
 	"github.com/praveenmahasena/generate_json/internal/jsonwriter"
 )
 
-func Run() error {
-	amount := cli.ReadAmount()
+// pretty sure logger is no more needed since I took off go routine
+// 15 mins later --
+// actually I'm wrong this is needed
+func logger() *slog.Logger {
 	w := io.MultiWriter(os.Stdout)
 	slogOpt := slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.Level(1),
 	}
 	l := slog.NewJSONHandler(w, &slogOpt)
-	sloger := slog.New(l)
-	js := jsonwriter.New(amount, sloger)
+	return slog.New(l)
+}
+
+func Run() error {
+	amount := cli.ReadAmount()
+	js := jsonwriter.New(amount, logger())
 	return js.Generate()
+}
+
+func Read() error {
+	fileRead, byteRead, err := jsonreader.Read(logger())
+	if err != nil {
+		return err
+	}
+	fmt.Printf("total files read %v \n", fileRead)
+	fmt.Printf("total bytes read %v \n", byteRead)
+	return nil
 }
