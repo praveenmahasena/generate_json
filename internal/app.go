f8eee1b138350ae31beba4861abab71bea72947d
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-10 17:37:14 -0400

    its 1:37AM and gotta wake up in 2:30Hrs idk what i just pushed

--- internal/app.go
+++ internal/app.go
@@ -1,10 +1,13 @@
 package internal
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/praveenmahasena/generate_json/internal/cli"
 	"github.com/praveenmahasena/generate_json/internal/jsonreader"
@@ -38,4 +41,27 @@ func Read() error {
 	fmt.Printf("total files read %v \n", fileRead)
 	fmt.Printf("total bytes read %v \n", byteRead)
 	return nil
+}
+
+// actually I'll be using go routine here
+// but not to read files
+// my brain is too tiny I cannot think of other ways :)
+// also channels for synced communication or data passing
+func GracefulRead() error {
+	sigCh := make(chan os.Signal, 1)
+	errCh := make(chan error)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // let's face it // I don't wanna have embarasing context leaks no fun for me
+
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT, syscall.SIGTRAP)
+
+	go func(ctx context.Context,err chan error) {
+		jsonreader.GracefulRead(ctx,logger(),err)
+	}(ctx,errCh)
+
+	<-sigCh
+	cancel()
+
+	return <-errCh
 }
--- internal/jsonreader/reader.go
+++ internal/jsonreader/reader.go
@@ -1,6 +1,7 @@
 package jsonreader
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -16,12 +17,14 @@ type (
 )
 
 // TODO: refactor get similar func to util package
+
 // I don't wanna do named return params
 // it's just not my style if it's nessocery please let me know
 // the 1st one is amount of files read
 // the 2st one is amount of file bytes read
 // the 3st one is any kind of error
 // I have created types for it on top
+
 func Read(l *slog.Logger) (fileRead, byteRead, error) {
 	wd, wdErr := os.Getwd()
 	if wdErr != nil {
@@ -35,10 +38,10 @@ func Read(l *slog.Logger) (fileRead, byteRead, error) {
 	if err != nil {
 		return 0, 0, fmt.Errorf("error during getting work dir %+v", wdErr)
 	}
-	fr:=fileRead(0)
-	br:=byteRead(0)
+	fr := fileRead(0)
+	br := byteRead(0)
 	for _, f := range files {
-		fr+=1
+		fr += 1
 		b, err := os.ReadFile(f.Name())
 		if err != nil {
 			errStr := fmt.Errorf("error during reading file %+v", err)
@@ -46,14 +49,28 @@ func Read(l *slog.Logger) (fileRead, byteRead, error) {
 			l.Error("error", errStr.Error(), "")
 			continue
 		}
-		br+=byteRead(len(b))
-		js:=jsonwriter.Js{}
-		if err:=json.Unmarshal(b,&js);err!=nil{
+		br += byteRead(len(b))
+		js := jsonwriter.Js{}
+		if err := json.Unmarshal(b, &js); err != nil {
 			// this error handling is not nessocery
 			// but still just gonna log into stdErr if json goes wrong that's all
-			errStr := fmt.Errorf("error during unmarshalling file %+v with value %+v",f, err)
+			errStr := fmt.Errorf("error during unmarshalling file %+v with value %+v", f, err)
 			l.Error("error", errStr.Error(), "")
 		}
 	}
 	return fr, br, nil
+}
+
+func GracefulRead(ctx context.Context, l *slog.Logger, errCh chan error) {
+	defer close(errCh)
+	wd, wdErr := os.Getwd()
+	if wdErr != nil {
+		errCh <- fmt.Errorf("error during getting working dir %+v", wdErr)
+		return
+	}
+	path := path.Join(wd, "/json/")
+	if err := os.Chdir(path); err != nil {
+		errCh <- fmt.Errorf("error during changing working dir %+v", err)
+		return
+	}
 }
