74f655f028bc6d178e744ad6f54c3f2916d5038a
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-14 11:48:07 -0400

    pushing with a bug

--- cmd/graceful_reader/main.go
+++ cmd/graceful_reader/main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/praveenmahasena/generate_json/internal"
+	"github.com/praveenmahasena/generate_json/internal/jsonreader"
+	"github.com/praveenmahasena/generate_json/internal/logger"
 )
 
-func main(){
-	if err:=internal.GracefulRead();err!=nil{
+func main() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	fileRead, byteRead, err := jsonreader.GracefulRead(sigCh,logger.Logger())
+
+	if err != nil {
 		fmt.Fprintln(os.Stderr,err)
+		// I did not wanted to return btw
+		// just print the error and then
+		// show out that it only read 0 values
 	}
+	fmt.Printf("total files read %v \n", fileRead)
+	fmt.Printf("total bytes read %v \n", byteRead)
 }
+
--- cmd/read_json/main.go
+++ cmd/read_json/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/praveenmahasena/generate_json/internal"
+	"github.com/praveenmahasena/generate_json/internal/jsonreader"
+	"github.com/praveenmahasena/generate_json/internal/logger"
 )
 
 func main() {
-	if err := internal.Read(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	fileRead, byteRead, err := jsonreader.Read(logger.Logger())
+	if err != nil {
+		fmt.Fprintln(os.Stderr,err)
+		// there is no a return statement here it's not an error
+		// I just wanna show it as the file read are 0
 	}
+	fmt.Printf("total files read %v \n", fileRead)
+	fmt.Printf("total bytes read %v \n", byteRead)
 }
