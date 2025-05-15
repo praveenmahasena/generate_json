cb000f51bca61c2b7f9ccab78c16c3dd1f1d0f1c
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-15 16:08:24 -0400

    done thanks Greg

--- cmd/graceful_reader/main.go
+++ cmd/graceful_reader/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"syscall"
 
 	"github.com/praveenmahasena/generate_json/internal/jsonreader"
 	"github.com/praveenmahasena/generate_json/internal/logger"
@@ -12,15 +11,10 @@ import (
 
 func main() {
 	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigCh, os.Interrupt)
 	fileRead, byteRead, err := jsonreader.GracefulRead(sigCh, logger.Logger())
-
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		// I did not wanted to return btw
-		// just print the error and then
-		// show out that it only read 0 values
+		fmt.Fprint(os.Stderr, err)
 	}
-	fmt.Printf("total files read %v \n", fileRead)
-	fmt.Printf("total bytes read %v \n", byteRead)
+	fmt.Println(fileRead, byteRead)
 }
