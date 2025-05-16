55aa5c975aca78209b66755f3cb47b356f0595f6
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-16 07:02:04 -0400

    adding in solution V1

--- internal/jsonreader/jsonreader.go
+++ internal/jsonreader/jsonreader.go
@@ -5,126 +5,117 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"log/slog"
 	"os"
-	"path"
 
-	"github.com/praveenmahasena/generate_json/internal/jsondelete"
 	"github.com/praveenmahasena/generate_json/internal/jsonwriter"
 )
 
 type (
-	fileRead uint
-	byteRead uint
+	filesRead uint
+	bytesRead uint
 )
 
-// TODO: refactor get similar func to util package
-
-// I don't wanna do named return params
-// it's just not my style if it's nessocery please let me know
-// the 1st one is amount of files read
-// the 2st one is amount of file bytes read
-// the 3st one is any kind of error
-// I have created types for it on top for this
-
-func Read(l *slog.Logger) (fileRead, byteRead, error) {
+func Read(l *slog.Logger) (filesRead, bytesRead, error) {
 	wd, wdErr := os.Getwd()
 	if wdErr != nil {
-		return 0, 0, fmt.Errorf("error during getting work dir %+v", wdErr)
+		return 0, 0, fmt.Errorf("error during getting work dir :%+v", wdErr)
 	}
-	path := path.Join(wd, "/json/")
-	if err := os.Chdir(path); err != nil {
-		return 0, 0, fmt.Errorf("error during enter work dir %+v", err)
+	if err := os.Chdir(wd + "/json/"); err != nil {
+		return 0, 0, fmt.Errorf("error during getting into json work dir :%+v", err)
 	}
-	files, err := os.ReadDir(".")
-	if err != nil {
-		return 0, 0, fmt.Errorf("error during getting work dir %+v", err)
+	files, filesErr := os.Open(".")
+	if filesErr != nil {
+		return 0, 0, fmt.Errorf("error during opening json work dir :%+v", filesErr)
 	}
-	fr := fileRead(0)
-	br := byteRead(0)
-	for _, f := range files {
-		fr += 1
-		b, err := os.ReadFile(f.Name())
+	defer files.Close()
+	fileNames, fileNameErr := files.Readdirnames(10_000_000_000) // and no this one is not gonna create an array with cap string 10M
+	// let's break this
+	// the fileNames is gonna have a arrayslice is gonna hold all the file names in the dir
+	// not being sorted
+	if fileNameErr != nil {
+		return 0, 0, fmt.Errorf("error during opening json work dir :%+v", filesErr)
+	}
+	var (
+		fileRead  filesRead = 0
+		byteRead  bytesRead = 0
+		fileBytes           = make([]byte, 50)
+		file      *os.File
+		err       error
+		n         int
+	)
+	for _, name := range fileNames {
+		file, err = os.Open(name)
 		if err != nil {
-			errStr := fmt.Errorf("error during reading file %+v", err)
-			// I'll be doing logging here for error handling since I do not see a purpose of bubbling up
-			l.Error("error", errStr.Error(), "")
+			log.Printf("error during opening file %v with value %+v", name, err)
+			file.Close()
 			continue
 		}
-		br += byteRead(len(b))
-		js := jsonwriter.Js{}
-		if err := json.Unmarshal(b, &js); err != nil {
-			// this error handling is not nessocery
-			// but still just gonna log into stdErr if json goes wrong that's all
-			errStr := fmt.Errorf("error during unmarshalling file %+v with value %+v", f, err)
-			l.Error("error", errStr.Error(), "")
+		n, err = io.ReadFull(file, fileBytes)
+		if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
+			log.Printf("error during reading file %v with value %+v", name, err)
+			file.Close()
+			continue
 		}
-		if err := jsondelete.DeleteFile(f.Name()); err != nil {
-			errStr := fmt.Errorf("error during deleting file %+v with value %+v", f.Name(), err)
-			l.Error("error", errStr.Error(), "")
+		fileRead += 1
+		byteRead += bytesRead(n)
+		file.Close()
+		js := &jsonwriter.Js{}
+		if err = json.Unmarshal(fileBytes[:n], js); err != nil {
+			log.Printf("error during unmarshal file %v with value %+v", name, err)
 		}
 	}
-	return fr, br, nil
+	return fileRead, byteRead, nil
 }
 
 // fileRead, byteRead, error
-func GracefulRead(sigCh chan os.Signal, l *slog.Logger) (uint, uint, error) {
+func GracefulRead(sigCh chan os.Signal, l *slog.Logger) (filesRead, bytesRead, error) {
 	wd, wdErr := os.Getwd()
 	if wdErr != nil {
-		return 0, 0, fmt.Errorf("error during getting work dir %+v", wdErr)
+		return 0, 0, fmt.Errorf("error during getting work dir :%+v", wdErr)
 	}
-	p := path.Join(wd, "/json/")
-	if err := os.Chdir(p); err != nil {
-		return 0, 0, fmt.Errorf("error during moving into json work dir %+v", err)
+	if err := os.Chdir(wd + "/json/"); err != nil {
+		return 0, 0, fmt.Errorf("error during getting into json work dir :%+v", err)
+	}
+	files, filesErr := os.Open(".")
+	if filesErr != nil {
+		return 0, 0, fmt.Errorf("error during opening json work dir :%+v", filesErr)
+	}
+	defer files.Close()
+	fileNames, fileNameErr := files.Readdirnames(10_000_000_000) // and no this one is not gonna create an array with cap string 10M
+	if fileNameErr != nil {
+		return 0, 0, fmt.Errorf("error during opening json work dir :%+v", filesErr)
 	}
 	var (
-		errCount      uint8
-		fileTrack     uint
-		fName         string
-		file          *os.File
-		err           error
-		buf           []byte = make([]byte, 100)
-		n             int
-		fileBytesRead uint
-		fileRead      uint
+		fileRead  filesRead = 0
+		byteRead  bytesRead = 0
+		fileBytes           = make([]byte, 50)
+		file      *os.File
+		err       error
+		n         int
 	)
-
-	for {
-		if len(sigCh) == 1 {
-			fmt.Println("cancelling...")
-			break
-		}
-		if errCount >= 1 {
-			break
-		}
-		fName, fileTrack = fmt.Sprintf("%v.json", fileTrack), fileTrack+1
-		file, err = os.OpenFile(fName, os.O_RDONLY, 0)
+	for _, name := range fileNames {
+		if len(sigCh)==1{break}
+		file, err = os.Open(name)
 		if err != nil {
-			l.Error("skipping file", fName, "")
-			err = nil
-			errCount += 1
+			log.Printf("error during opening file %v with value %+v", name, err)
+			file.Close()
 			continue
 		}
-		n, err = io.ReadFull(file, buf)
+		n, err = io.ReadFull(file, fileBytes)
 		if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
-			l.Error("skipping file", fName, "")
-			err = nil
+			log.Printf("error during reading file %v with value %+v", name, err)
+			file.Close()
 			continue
 		}
-		fileBytesRead += uint(n)
 		fileRead += 1
+		byteRead += bytesRead(n)
 		file.Close()
 		js := &jsonwriter.Js{}
-
-		err = json.Unmarshal(buf[:n], js)
-		if err != nil {
-			l.Error("error during unmarshal json", err.Error(), "")
-			err = nil
-		}
-		if err = jsondelete.DeleteFile(fName); err != nil {
-			l.Error("error during deleting json file", err.Error(), "")
-			err = nil
+		if err = json.Unmarshal(fileBytes[:n], js); err != nil {
+			log.Printf("error during unmarshal file %v with value %+v", name, err)
 		}
 	}
-	return fileRead, fileBytesRead, nil
+	return fileRead, byteRead, nil
 }
