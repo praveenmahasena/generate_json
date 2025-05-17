4b6be5d80862b9e9af66ec71ac438048eba909d1
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-17 18:23:47 -0400

    adding in

--- cmd/generate_json/main.go
+++ cmd/generate_json/main.go
@@ -1,73 +1,82 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/praveenmahasena/generate_json/internal"
+	"syscall"
 )
 
 var (
-	defaultAmount uint = 10_000
+	defaultAmount      uint = 10_000
+	defaultDirectories uint = 1
 )
 
 type Jsonwriter struct {
-	Amount uint
+	amount      uint
+	directories uint
 }
 
 func main() {
-	amount := readAmount()
-	js := new(amount)
+	amount, directories := readAmount()
+	js := new(amount, directories)
 	if err := js.generate(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
-func new(id uint) *Jsonwriter {
-	return &Jsonwriter{id}
+func new(id, directories uint) *Jsonwriter {
+	return &Jsonwriter{id, directories}
 }
 
-func readAmount() uint {
+func readAmount() (uint, uint) {
 	amount := flag.Uint("amount", defaultAmount, "amount of json files this binary should generate would be defaulted to 10_000")
+	directories := flag.Uint("directories", defaultDirectories, "amount of json directories this binary should generate would be defaulted to 1")
 	flag.Parse()
-	return *(amount) // call me old idc but been doing Clang for awhile and this *() reference seems more appealing then just doing *
+	return *(amount), *(directories) // call me old idc but been doing Clang for awhile and this *() reference seems more appealing then just doing *
 }
 
 func (j *Jsonwriter) generate() error {
-	dir, dirErr := os.Getwd()
-	if dirErr != nil {
-		return fmt.Errorf("error during getting work dir %+v", dirErr)
+	if err := os.RemoveAll("json"); err != nil {
+		return fmt.Errorf("error during removing previous json dir %+v ", err)
 	}
-	path := dir + "/json/"
-	if err := os.RemoveAll(path); err != nil {
-		return fmt.Errorf("error during removing existing pre generated json dir %+v ", err)
+	if err := os.Mkdir("json", 0777); err != nil {
+		return fmt.Errorf("error during making dir for json %+v", err)
 	}
-	if err := os.Mkdir(path, 0777); err != nil {
-		return fmt.Errorf("error during creating json dir %+v", err)
+	if err := os.Chdir("./json"); err != nil {
+		return fmt.Errorf("error during moving into dir for json with value %v ,%+v", "json", err)
 	}
-	if err := os.Chdir(path); err != nil {
-		return fmt.Errorf("error during changing to json dir %+v", err)
-	}
-	for i := range j.Amount {
-		if err := writeFile(i); err != nil {
-			log.Printf("file write error %v", err)
+	for d := range j.directories {
+		dirName := fmt.Sprintf("%vjson", d)
+		if err := os.Mkdir(dirName, 0777); err != nil {
+			log.Printf("error during making dir for json %+v", err)
+			continue
+		}
+		if err := syscall.Chdir("./" + dirName); err != nil {
+			log.Printf("error during moving into dir for json with value %v ,%+v", dirName, err)
+			continue
+		}
+
+		for a := range j.amount {
+			if err := writeFile(a); err != nil {
+				log.Println()
+			}
+		}
+		if err := syscall.Chdir("../"); err != nil {
+			return fmt.Errorf("error during moving moving dir in json %+v", err)
 		}
 	}
 	return nil
 }
 
 func writeFile(i uint) error {
-	fileName := fmt.Sprintf("%v.json", i)
-	content, err := json.MarshalIndent(internal.Js{i}, " ", "")
-	if err != nil {
-		return fmt.Errorf("error during generating json %+v", err)
-	}
-	fileErr := os.WriteFile(fileName, content, 0644)
-	if fileErr != nil {
-		return fmt.Errorf("error during writing into json file %+v", fileErr)
+	fName := fmt.Sprintf("%v.json", i)
+	b := `{
+		"id":`+fmt.Sprintf("%v",i)+`
+	}`
+	if err := os.WriteFile(fName, []byte(b), 0666); err != nil {
+		return fmt.Errorf("error during writing into file %v with value %+v", fName, err)
 	}
 	return nil
 }
--- cmd/graceful_reader/main.go
+++ cmd/graceful_reader/main.go
@@ -16,69 +16,86 @@ import (
 func main() {
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
-	fileRead, byteRead, err := gracefulRead(sigCh)
+	dirRead, fileRead, byteRead, err := gracefulRead(sigCh)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
 	}
-	fmt.Println(fileRead, byteRead)
+	fmt.Printf("total dir read %v \n", dirRead)
+	fmt.Printf("total files read %v \n", fileRead)
+	fmt.Printf("total bytes read %v \n", byteRead)
 }
 
-func gracefulRead(sigCh chan os.Signal) (uint, uint, error) {
-	wd, wdErr := os.Getwd()
-	if wdErr != nil {
-		return 0, 0, fmt.Errorf("error during getting work dir :%+v", wdErr)
+func gracefulRead(sigCh chan os.Signal) (uint, uint, uint, error) {
+	if err := os.Chdir("./json"); err != nil {
+		return 0, 0, 0, fmt.Errorf("cannot move into json dir %+v", err)
 	}
-	if err := os.Chdir(wd + "/json/"); err != nil {
-		return 0, 0, fmt.Errorf("error during getting into json work dir :%+v", err)
+	file, fileErr := os.Open(".")
+	if fileErr != nil {
+		return 0, 0, 0, fmt.Errorf("cannot open dir json %+v", fileErr)
 	}
-	files, filesErr := os.Open(".")
-	if filesErr != nil {
-		return 0, 0, fmt.Errorf("error during opening json work dir :%+v", filesErr)
-	}
-	defer files.Close()
-	fileNames, fileNameErr := files.Readdirnames(10_000_000_000) // and no this one is not gonna create an array with cap string 10M
-	if fileNameErr != nil {
-		return 0, 0, fmt.Errorf("error during opening json work dir :%+v", filesErr)
+	defer file.Close()
+	dirNames, dirNameErr := file.Readdirnames(100)
+	if dirNameErr != nil {
+		return 0, 0, 0, fmt.Errorf("cannot read json dir names %+v", dirNameErr)
 	}
 	var (
-		fileRead  uint = 0
-		byteRead  uint = 0
-		fileBytes      = make([]byte, 50)
-		file      *os.File
-		err       error
-		n         int
+		dirRead  uint
+		byteRead uint
+		fileRead uint
 	)
-	for _, name := range fileNames {
-		if len(sigCh) == 1 {
-			break
+	for _, dir := range dirNames {
+		if len(sigCh)==1{break}
+		f, fErr := os.Open("./" + dir)
+		if fErr != nil {
+			log.Printf("error during opening in dir %v with value %+v", dir, fErr)
+			continue
 		}
-		file, err = os.Open(name)
+		fileNames, err := f.Readdirnames(10_000)
 		if err != nil {
-			log.Printf("error during opening file %v with value %+v", name, err)
-			file.Close()
+			log.Printf("error during reading all json file name in dir %v with value %+v", dir, err)
+			f.Close()
 			continue
 		}
-		n, err = io.ReadFull(file, fileBytes)
-		if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
-			log.Printf("error during reading file %v with value %+v", name, err)
-			file.Close()
-			continue
-		}
-		fileRead += 1
-		byteRead += uint(n)
-		file.Close()
-		js := &internal.Js{}
-		if err = json.Unmarshal(fileBytes[:n], js); err != nil {
-			log.Printf("error during unmarshal file %v with value %+v", name, err)
-		}
-		deleteFile(name)
+		br, fr := readDirFiles(fileNames, dir,sigCh)
+		f.Close()
+		byteRead += br
+		fileRead += fr
+		dirRead += 1
+		deleteFile(dir)
 	}
-	return fileRead, byteRead, nil
+	return dirRead, fileRead, byteRead, nil
 }
 
+func readDirFiles(fileNames []string, dirName string,sigCh chan os.Signal) (uint, uint) {
+	if err := os.Chdir(dirName); err != nil {
+		log.Printf("error during moving into %v with %+v", dirName, err)
+		return 0, 0
+	}
+	defer os.Chdir("../")
+	buf := make([]byte, 50)
+	var fr uint
+	var br uint
+	for _, fn := range fileNames {
+		if len(sigCh)==1{break}
+		f, e := os.OpenFile(fn,os.O_RDONLY,0666)
+		if e != nil {
+			log.Printf("error during opening up file %v in dir %v with %+v", dirName, fn, e)
+			continue
+		}
+		n, err := io.ReadFull(f, buf)
+		f.Close()
+		if err != nil && !errors.Is(err,io.ErrUnexpectedEOF) {
+//			log.Printf("error during reading file %v in dir %v with %+v", dirName, fn, err)
+		}
+		fr += 1
+		br += uint(n)
+		json.Unmarshal(buf[:n],&internal.Js{})
+	}
+	return br, fr
+}
 func deleteFile(fName string) error {
 	if err := syscall.Unlink(fName); err != nil {
-		return fmt.Errorf("error during deleting file %+v",fName)
+		return fmt.Errorf("error during deleting file %+v", fName)
 	}
 	return nil
 }
