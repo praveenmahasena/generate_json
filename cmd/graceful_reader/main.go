91d33f9eecfc79df756b09aefb79d5e0ce47ef03
praveenmahasena <praveenmahasena647@gmail.com>
2025-05-22 06:57:50 -0400

    adding in something

--- cmd/graceful_reader/main.go
+++ cmd/graceful_reader/main.go
@@ -5,11 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"log/slog"
 	"os"
 	"os/signal"
-	"path"
 
 	"github.com/praveenmahasena/generate_json/internal"
 )
@@ -18,101 +16,83 @@ func main() {
 	l := internal.NewLogger(os.Stdout, true, 1)
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
-	fileRead, byteRead, err := gracefulRead(sigCh, l)
+	fileRead, bytesRead, err := gracefulRead(sigCh, l)
 	if err != nil {
-		fmt.Fprint(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, err)
 	}
-	fmt.Printf("total files read %v \n", fileRead)
-	fmt.Printf("total bytes read %v \n", byteRead)
+	fmt.Printf("total file read %v", fileRead)
+	fmt.Printf("total bytes read %v", bytesRead)
 }
 
 func gracefulRead(sigCh chan os.Signal, l *slog.Logger) (uint, uint, error) {
-	dir, dirErr := os.Open("./json")
-	if dirErr != nil {
-		return 0, 0, fmt.Errorf("error during opening up %v with value %+v", "./json", dirErr)
+	jsonDir, jsonDirErr := os.Open("./json")
+	if jsonDirErr != nil {
+		return 0, 0, fmt.Errorf("error during opeing ./json dir with value %+v", jsonDirErr)
 	}
-	defer dir.Close()
+	defer jsonDir.Close()
+
 	var (
 		fileRead  uint
 		bytesRead uint
 	)
+
 	for {
-		if len(sigCh) == 1 {
-			l.Info("cancelling due to signal")
+		if len(sigCh)==1{
+			l.Error("closing off syscall.SIGINT")
 			break
 		}
-		dirNames, dirNamesErr := dir.Readdirnames(-1)
-		if dirNamesErr != nil && !errors.Is(dirNamesErr, io.EOF) {
-			return fileRead, bytesRead, fmt.Errorf("error during reading out directories with value %+v", dirNamesErr)
-		}
-		for _, jDir := range dirNames {
-			f, b, err := readSubDir(jDir, sigCh, l)
-			if err != nil {
-				log.Println(err)
+		subDirNames, subDirNameErr := jsonDir.Readdirnames(10)
+		if subDirNameErr != nil {
+			if errors.Is(subDirNameErr, io.EOF) {
+				l.Info("all files processed")
+				break
 			}
-			fileRead += f
-			bytesRead += b
-		}
-
-		if len(dirNames) < 10 {
-			l.Info("all dir read...")
-			break
-		}
-	}
-	// I can do somekind of bubble up value and make the "./json" dir get deleted but i do not think it's that important since all the other dirs are getting cleaned up :)
-	return fileRead, bytesRead, nil
-}
-
-func readSubDir(jDir string, sigCh chan os.Signal, l *slog.Logger) (uint, uint, error) {
-	p := path.Join("./json", jDir, "/")
-	dir, dirErr := os.Open(p)
-	if dirErr != nil {
-		return 0, 0, fmt.Errorf("error during opening up %v with value %+v", p, dirErr)
-	}
-	defer dir.Close() // Im gonna delete the dir if the file amount I got is == 0 if I do this while having this in a defer stack would get me an error?
-	// for safety reason I'll have some more close statements in places
-	// yes closing a closed *os.File is gonna get me error but guess what this kind of error is meaningless
-	// so I do not have to manage it
-	dirNames, dirNameErr := dir.Readdirnames(-1)
-	if dirNameErr != nil && !errors.Is(dirNameErr,io.EOF){
-		return 0, 0, fmt.Errorf("error during getting up %v all the files with value of %+v", p, dirNameErr)
-	}
-	var (
-		failedFiles uint
-		fileRead    uint
-		bytesRead   uint
-	)
-	for _, name := range dirNames {
-		if len(sigCh) == 1 {
-			break
-		}
-		b, err := os.ReadFile(p + "/" + name)
-		if err != nil {
-			failedFiles += 1
-			l.Error("error during read file", "file name", p+"/"+name, "error value", err)
+			l.Error("error during reading ./json sub dirs", "error value", subDirNameErr, "process", "skipping...")
 			continue
 		}
-		if err := json.Unmarshal(b, &internal.Js{}); err != nil {
-			failedFiles += 1
-			l.Error("error during decoding json into file", "file name", p+"/"+name, "error value", err)
-			continue
+		inner:
+		for _, subDirs := range subDirNames {
+			if len(sigCh)==1{
+				break inner
+			}
+			p := "./json/" + subDirs
+			subDirNames, subDirNamesErr := os.Open(p)
+			if subDirNamesErr != nil {
+				l.Error("error during opening dir", "dir name", p, "error value", subDirNamesErr, "process", "skipping...")
+				continue inner
+			}
+			deeper:
+			for {
+				if len(sigCh)==1{
+					break deeper
+				}
+				fileCollection, fileCollectionErr := subDirNames.Readdirnames(10)
+				if fileCollectionErr != nil {
+					if errors.Is(fileCollectionErr, io.EOF) {
+						break deeper
+					}
+					l.Error("error during file name read", "dir name", p, "error value", fileCollectionErr, "process", "skipping...")
+					continue deeper
+				}
+				fileLoop:
+				for _, fileName := range fileCollection {
+					if len(sigCh)==1{
+						break fileLoop
+					}
+					b, err := os.ReadFile(p + "/" + fileName)
+					if err != nil {
+						l.Error("error during reading file", "file name", p+"/"+fileName, "error value", err, "process", "skipping...")
+						continue fileLoop
+					}
+					if err := json.Unmarshal(b, &internal.Js{}); err != nil {
+						l.Error("error during marshelling file", "file name", p+"/"+fileName, "error value", err, "process", "skipping...")
+						continue fileLoop
+					}
+					fileRead += 1
+					bytesRead += uint(len(b))
+				}
+			}
 		}
-		fileRead += 1
-		bytesRead += uint(len(b))
-	}
-	if failedFiles > 0 {
-		return fileRead, bytesRead, fmt.Errorf("dir %v coulndnt be deleted", p)
-	}
-	dir.Close()
-	if err := deleteAll(p); err != nil {
-		return fileRead, bytesRead, fmt.Errorf("error during deleting dir %v with value %+v", p, err)
 	}
 	return fileRead, bytesRead, nil
-}
-
-func deleteAll(fName string) error {
-	if err := os.RemoveAll(fName); err != nil {
-		return fmt.Errorf("error during deleting file %+v", fName)
-	}
-	return nil
 }
